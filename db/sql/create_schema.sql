c7db6762a3c845581be20c357324047b2ce1698e
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2024-03-05 11:01:25 -0300

    Sql files are added

---
+++ db/sql/create_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE product (
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(255),
+                         price DECIMAL(10, 2),
+                         stock INT,
+                         category INT
+);
---
+++ db/sql/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO product (name, price, stock, category) VALUES ('Producto A', 10.50, 100, 1);
+INSERT INTO product (name, price, stock, category) VALUES ('Producto B', 20.75, 150, 2);
+INSERT INTO product (name, price, stock, category) VALUES ('Producto C', 15.99, 80, 1);
+INSERT INTO product (name, price, stock, category) VALUES ('Producto D', 5.25, 200, 3);
