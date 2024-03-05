d3ce02607cf8505f99cc3d4e5680de21af3ad1e6
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2024-03-05 11:29:24 -0300

    GlobalConstants is added

---
+++ src/main/java/com/bricks/challanger/utils/GlobalConstants.java
@@ -0,0 +1,6 @@
+package com.bricks.challanger.utils;
+
+public class GlobalConstants {
+    public static final String PRODUCT_TABLE = "products";
+    public static final String CATEGORY_TABLE = "categories";
+}
