657cc6279fcf2a0d4142ecc6d3e8378fcb95b1d9
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2024-03-05 11:04:44 -0300

    Init app is added

---
+++ src/test/java/com/bricks/challanger/BricksChallangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bricks.challanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BricksChallangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
