59ff3fb3a3ce3ae1a335a752cd86d4ed46573100
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2024-03-06 11:37:16 -0300

    Tests are added

--- src/test/java/com/bricks/challanger/services/impl/ProductServiceImplTest.java
+++ src/test/java/com/bricks/challanger/services/impl/ProductServiceImplTest.java
@@ -1,9 +1,14 @@
 package com.bricks.challanger.services.impl;
 
+import com.bricks.challanger.repositories.CategoryRepository;
 import com.bricks.challanger.repositories.ProductRepository;
 import com.bricks.challanger.services.CategoryService;
 import com.bricks.challanger.services.ProductService;
 import com.bricks.challanger.util.Data;
+import com.bricks.challanger.utils.GlobalConstants;
+import com.bricks.challanger.utils.exceptions.CategoriesNotFoundException;
+import com.bricks.challanger.utils.exceptions.IdNotFoundException;
+import com.bricks.challanger.utils.exceptions.NameNotFoundException;
 import com.bricks.challanger.utils.mappers.CategoryMapper;
 import com.bricks.challanger.utils.mappers.ProductMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +22,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.Objects;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -43,6 +49,9 @@ class ProductServiceImplTest {
 
     @MockBean
     private CategoryMapper categoryMapper;
+
+    @MockBean
+    private CategoryRepository categoryRepository;
 
     @Test
     @DisplayName("Searching for a product list is tested")
@@ -77,6 +86,21 @@ class ProductServiceImplTest {
     }
 
     @Test
+    @DisplayName("Create a product and fail is tested")
+    void createdException() {
+        // ASSEMBLE
+        when(categoryRepository.findByNameAndCode(any(), any())).thenReturn(null);
+        when(categoryService.findByNameAndCode(any(), any())).thenThrow(new CategoriesNotFoundException("Categories not found"));
+        when(productRepository.save(any())).thenReturn(Data.PRODUCT);
+        when(mapper.toModel(any())).thenReturn(Data.PRODUCT_DTO);
+
+        // ACT and ASSERT
+        var exception = assertThrows(CategoriesNotFoundException.class, () -> productService.created(Data.PRODUCT_DTO));
+        String result = exception.getMessage();
+        assertEquals("Exception fail","Categories not found", result);
+    }
+
+    @Test
     @DisplayName("Read a product is tested")
     void read() {
         //ASSEMBLE
@@ -88,8 +112,21 @@ class ProductServiceImplTest {
 
         //ASSERT
         assertTrue("Products is null", Objects.nonNull(product));
-        assertEquals("Producto not found", "Product 1",product.getName());
+        assertEquals("Product not found", "Product 1",product.getName());
     }
+
+    @Test
+    @DisplayName("Read a product fail is tested")
+    void readFail() {
+        //ASSEMBLE
+        when(productRepository.findByIdAndState(any(),any())).thenThrow(new IdNotFoundException(GlobalConstants.PRODUCT_TABLE));
+
+        // ACT and ASSERT
+        var exception = assertThrows(IdNotFoundException.class, () -> productService.read(120L));
+        String result = exception.getMessage();
+        assertEquals("Exception fail","Record no exist in products", result);
+    }
+
 
     @Test
     @DisplayName("Updated a product is tested")
@@ -108,6 +145,19 @@ class ProductServiceImplTest {
         assertEquals("Producto not found", "Product 1",product.getName());
     }
 
+
+    @Test
+    @DisplayName("Update a product fail is tested")
+    void updateFail() {
+        //ASSEMBLE
+        when(productRepository.findByIdAndState(any(),any())).thenThrow(new IdNotFoundException(GlobalConstants.PRODUCT_TABLE));
+
+        // ACT and ASSERT
+        var exception = assertThrows(NameNotFoundException.class, () -> productService.update(Data.PRODUCT_DTO));
+        String result = exception.getMessage();
+        assertEquals("Exception fail","Record no exist in products", result);
+    }
+
     @Test
     @DisplayName("Delete a product is tested")
     void delete() {
@@ -121,4 +171,5 @@ class ProductServiceImplTest {
         verify(productRepository).findById(1L);
 
     }
+
 }
